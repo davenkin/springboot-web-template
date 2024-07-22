55b1ab8232121d69ca31c688ad994704d84f7a70
Davenkin <davenkin@126.com>
2024-07-22 14:50:34 +0800

    Update HelloWorldController.java

--- src/main/java/davenkin/springboot/web/template/helloworld/HelloWorldController.java
+++ src/main/java/davenkin/springboot/web/template/helloworld/HelloWorldController.java
@@ -1,17 +1,16 @@
 package davenkin.springboot.web.template.helloworld;
 
-import org.springframework.http.HttpMethod;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
-import java.util.Map;
-
 @RestController
 public class HelloWorldController {
 
-    @GetMapping(value = "/hello-world")
-    public Map<String, String> helloWorld() throws NoResourceFoundException {
-        return Map.of("value", "Hello World!");
-    }
+  @GetMapping(value = "/hello-world")
+  public Map<String, String> helloWorld() throws NoResourceFoundException {
+    return Map.of("value", "Hello World!");
+  }
 }
