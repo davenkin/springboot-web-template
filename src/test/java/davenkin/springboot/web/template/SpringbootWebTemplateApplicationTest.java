3eefe159ecad91bb9d1f49d61af37c6ea2645480
Yun Yun4 Teng <tengyun4@lenovo.com>
2024-07-22 12:00:15 +0800

    format

--- src/test/java/davenkin/springboot/web/template/SpringbootWebTemplateApplicationTest.java
+++ src/test/java/davenkin/springboot/web/template/SpringbootWebTemplateApplicationTest.java
@@ -1,32 +1,31 @@
 package davenkin.springboot.web.template;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-import java.util.Map;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 class SpringbootWebTemplateApplicationTest {
 
-    @Autowired
-    private WebTestClient webTestClient;
+  @Autowired
+  private WebTestClient webTestClient;
 
-    @Test
-    void shouldSayHelloWorld() {
-        var result = this.webTestClient.get()
-                .uri("/hello-world")
-                .exchange()
-                .expectStatus()
-                .is2xxSuccessful()
-                .expectBody(Map.class)
-                .returnResult()
-                .getResponseBody();
-        assertEquals("Hello World!", result.get("value"));
-    }
-
+  @Test
+  void shouldSayHelloWorld() {
+    var result = this.webTestClient.get()
+        .uri("/hello-world")
+        .exchange()
+        .expectStatus()
+        .is2xxSuccessful()
+        .expectBody(Map.class)
+        .returnResult()
+        .getResponseBody();
+    assertEquals("Hello World!", result.get("value"));
+  }
 }
