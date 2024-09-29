39b50542df7498b3663e16a42ab6795066b31d97
Teng Yun <davenkin@126.com>
2024-09-29 21:20:35 +0800

    refactor

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 ### How to run
 - `./mvnw spring-boot:run` and open `http://localhost:8080/hello-world`
-- Or run `main()` in `SpringbootWebTemplateApplication` in IDE directly
+- Or run `main()` in `SpringBootWebApplication` in IDE directly
 
 ### Tech stack
 - Spring Boot 3.3.2
--- src/test/java/davenkin/springboot/web/template/SpringBootWebApplicationTest.java
+++ src/test/java/davenkin/springboot/web/template/SpringBootWebApplicationTest.java
@@ -11,7 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-class SpringbootWebTemplateApplicationTest {
+class SpringBootWebApplicationTest {
 
   @Autowired
   private WebTestClient webTestClient;
